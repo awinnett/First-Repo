1130ad33177f259a7a2ff5f996ccbbff86d66b03
awinnett <winnett.alex@gmail.com>
2014-10-21 16:51:42 -0400

    very important goldfish test
    super important

---
+++ goldfish.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 21 16:49:40 2014
+
+@author: Alex
+"""
+
+goldfish = ["blue", "gold", "red"]
+
+for color in goldfish:
+    if color == "gold":
+        print "goldfish are gold"
